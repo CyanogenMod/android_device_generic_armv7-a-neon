993e411889f05caee191cf3c04dbd6aa9d812c37
keunyoung <keunyoung@google.com>
2013-03-05 14:46:37 -0800

    add TARGET_CPU_VARIANT to PDK ARM target
    - fix build
    
    Change-Id: I0af2d9961445b34b2ef2a5f38014ed20f9602f7f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -9,6 +9,7 @@ TARGET_NO_KERNEL := true
 
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_VARIANT := cortex-a9
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
